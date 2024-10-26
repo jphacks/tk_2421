1a23b35d2f82fe57b517f04251beae75c6a644e5
blue031415 <blue031415@gmail.com>
2024-10-26 12:09:58 +0900

    setting .env

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,33 @@
 // src/App.jsx
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
-    fetch('/api/hello') // Express APIエンドポイントを呼び出す
+    fetch("/api/hello") // Express APIエンドポイントを呼び出す
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
       .then((data) => setMessage(data.message))
-      .catch((error) => console.error('Error:', error));
-  }, []);
+      .catch((error) => console.error("Error:", error));
+  });
 
   return (
     <div>
-      <h1>{message || 'Loading...'}</h1>
+      <h1>{message || "Loading..."}</h1>
     </div>
   );
 }
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly SUPABASE_KEY: string;
+  readonly SUPABASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
