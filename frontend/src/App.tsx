9226000678a47b367919b3470ac36f6da3e34001
blue031415 <blue031415@gmail.com>
2024-10-26 17:13:32 +0900

    a

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './assets/style/style.css';
-import EntryForm from './components/EntryForm';
-import Tournament from './components/Tournament';
-import Menu from './components/Menu';
-import AuthForm from './components/AuthForm';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./assets/style/style.css";
+import EntryForm from "./components/EntryForm";
+import Tournament from "./components/Tournament";
+import Menu from "./components/Menu";
+import AuthForm from "./components/AuthForm";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AuthForm />}/>
-        <Route path="/menu" element={<Menu />}/>
-        <Route path="/show-tournament" element={<Tournament />}/>
+        <Route path="/" element={<AuthForm />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/show-tournament" element={<Tournament />} />
         <Route path="/entry" element={<EntryForm />} />
       </Routes>
     </BrowserRouter>
